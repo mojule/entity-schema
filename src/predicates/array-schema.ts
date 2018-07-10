d1067ece18547fa5ad17e4c13938198e43edfafc
Nik Coughlin <nrkn.com@gmail.com>
2018-07-10 17:03:19 +1200

    Fix bad references

--- src/predicates/array-schema.ts
+++ src/predicates/array-schema.ts
@@ -1,5 +1,5 @@
 import { TSubschema } from './subschema'
-import { JSONSchema4 } from '../../node_modules/@types/json-schema'
+import { JSONSchema4 } from 'json-schema'
 
 export interface IArraySchema extends JSONSchema4 {
   type: 'array'
--- src/predicates/boolean-schema.ts
+++ src/predicates/boolean-schema.ts
@@ -1,4 +1,4 @@
-import { JSONSchema4 } from '../../node_modules/@types/json-schema';
+import { JSONSchema4 } from 'json-schema'
 
 export interface IBooleanSchema extends JSONSchema4 {
   type: 'boolean'
